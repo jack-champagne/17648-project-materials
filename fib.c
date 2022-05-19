418a1b31cf687734d6308895778ed750e2d32f07
Malcadore <48132207+Malcadore@users.noreply.github.com>
2022-05-19 19:26:06 -0400

    Update fib.c
    Added function and file headers.

--- fib.c
+++ fib.c
@@ -1,6 +1,18 @@
+/* fib.c
+* Author:Scott Pavetti
+* Date: 5/19/2022
+* Simple source file used to test out
+* automated builds and other basic things.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+* fib
+* Recursive fibonacci function.
+* No error checking whatsoever.
+*/
 int fib(int n) {
     if (n <= 2){
         return 1;
@@ -8,6 +20,11 @@ int fib(int n) {
     return fib(n-1) + fib(n-2);
 }
 
+/* main
+* Takes in a number that sets up the 
+* fibonacci function to operate. Could
+* use some error checking.
+*/
 int main(int argc, char** argv) {
     if (argc < 2) {
         fprintf(stderr, "Error: Insufficient arguments.\n");
